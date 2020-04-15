53f2a2c234a21b5edb4922f7e70df2c364a3b9ec
milad <milad.geranmayeh2@gmail.com>
2020-04-15 23:54:05 +0430

    add Timeout for dialog

--- app/src/main/java/com/android/tu/mloadingdialog/MainActivity.java
+++ app/src/main/java/com/android/tu/mloadingdialog/MainActivity.java
@@ -9,53 +9,42 @@ import android.widget.Button;
 
 import com.android.tu.loadingdialog.LoadingDialog;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btn1,btn2;
+    Button btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn1= (Button) findViewById(R.id.style1_btn);
-        btn2= (Button) findViewById(R.id.style2_btn);
+        btn1 = (Button) findViewById(R.id.style1_btn);
+        btn2 = (Button) findViewById(R.id.style2_btn);
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.style1_btn:
-                LoadingDialog.Builder builder1=new LoadingDialog.Builder(MainActivity.this)
+                LoadingDialog.Builder builder1 = new LoadingDialog.Builder(MainActivity.this)
                         .setMessage("加载中...")
-                        .setCancelable(false);
-                final LoadingDialog dialog1=builder1.create();
+                        .setMaxWaitTime(8)
+                        .setCancelable(true);
+                final LoadingDialog dialog1 = builder1.create();
                 dialog1.show();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        dialog1.dismiss();
-                    }
-                },2000);
                 break;
             case R.id.style2_btn:
-                LoadingDialog.Builder builder2=new LoadingDialog.Builder(MainActivity.this)
+                LoadingDialog.Builder builder2 = new LoadingDialog.Builder(MainActivity.this)
                         .setShowMessage(false)
-                        .setCancelable(false);
-                final LoadingDialog dialog2=builder2.create();
+                        .setCancelable(true);
+                final LoadingDialog dialog2 = builder2.create();
                 dialog2.show();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        dialog2.dismiss();
-                    }
-                },2000);
                 break;
         }
     }
 
-    private Handler handler=new Handler(){
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
--- loadingdialog/src/main/java/com/android/tu/loadingdialog/LoadingDialog.java
+++ loadingdialog/src/main/java/com/android/tu/loadingdialog/LoadingDialog.java
@@ -2,15 +2,22 @@ package com.android.tu.loadingdialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.logging.LogRecord;
+
 /**
  * Created by tjy on 2017/6/19.
  */
-public class LoadingDialog extends Dialog{
+public class LoadingDialog extends Dialog {
 
+
+    private Handler handler;
+    private Runnable autoCloseRunnable;
+    private int waitTime;
 
     public LoadingDialog(Context context) {
         super(context);
@@ -20,13 +27,15 @@ public class LoadingDialog extends Dialog{
         super(context, themeResId);
     }
 
-    public static class Builder{
+
+    public static class Builder {
 
         private Context context;
         private String message;
-        private boolean isShowMessage=true;
-        private boolean isCancelable=false;
-        private boolean isCancelOutside=false;
+        private boolean isShowMessage = true;
+        private boolean isCancelable = false;
+        private boolean isCancelOutside = false;
+        private int maxWaitTime = 15000;
 
 
         public Builder(Context context) {
@@ -35,64 +44,114 @@ public class LoadingDialog extends Dialog{
 
         /**
          * 设置提示信息
+         *
          * @param message
          * @return
          */
 
-        public Builder setMessage(String message){
-            this.message=message;
+        public Builder setMessage(String message) {
+            this.message = message;
             return this;
         }
 
         /**
          * 设置是否显示提示信息
+         *
          * @param isShowMessage
          * @return
          */
-        public Builder setShowMessage(boolean isShowMessage){
-            this.isShowMessage=isShowMessage;
+        public Builder setShowMessage(boolean isShowMessage) {
+            this.isShowMessage = isShowMessage;
             return this;
         }
 
         /**
          * 设置是否可以按返回键取消
+         *
          * @param isCancelable
          * @return
          */
 
-        public Builder setCancelable(boolean isCancelable){
-            this.isCancelable=isCancelable;
+        public Builder setCancelable(boolean isCancelable) {
+            this.isCancelable = isCancelable;
+            return this;
+        }
+
+        public Builder setMaxWaitTime(int seconds) {
+            this.maxWaitTime = seconds * 1000;
             return this;
         }
 
         /**
          * 设置是否可以取消
+         *
          * @param isCancelOutside
          * @return
          */
-        public Builder setCancelOutside(boolean isCancelOutside){
-            this.isCancelOutside=isCancelOutside;
+        public Builder setCancelOutside(boolean isCancelOutside) {
+            this.isCancelOutside = isCancelOutside;
             return this;
         }
 
-        public LoadingDialog create(){
+        public LoadingDialog create() {
 
             LayoutInflater inflater = LayoutInflater.from(context);
-            View view=inflater.inflate(R.layout.dialog_loading,null);
-            LoadingDialog loadingDailog=new LoadingDialog(context,R.style.MyDialogStyle);
-            TextView msgText= (TextView) view.findViewById(R.id.tipTextView);
-            if(isShowMessage){
+            View view = inflater.inflate(R.layout.dialog_loading, null);
+            LoadingDialog loadingDailog = new LoadingDialog(context, R.style.MyDialogStyle);
+            TextView msgText = (TextView) view.findViewById(R.id.tipTextView);
+            if (isShowMessage) {
                 msgText.setText(message);
-            }else{
+            } else {
                 msgText.setVisibility(View.GONE);
             }
             loadingDailog.setContentView(view);
             loadingDailog.setCancelable(isCancelable);
             loadingDailog.setCanceledOnTouchOutside(isCancelOutside);
-            return  loadingDailog;
+            loadingDailog.enableTimer(maxWaitTime);
+            return loadingDailog;
 
         }
 
 
+    }
+
+    private void enableTimer(int time) {
+
+        this.waitTime = time;
+        handler = new Handler();
+
+        autoCloseRunnable = new Runnable() {
+            @Override
+            public void run() {
+                dismiss();
+            }
+        };
+    }
+
+    @Override
+    public void show() {
+        super.show();
+
+        if (handler != null && autoCloseRunnable != null) {
+            handler.postDelayed(autoCloseRunnable, waitTime);
+        }
+    }
+
+    @Override
+    public void dismiss() {
+        super.dismiss();
+        if (handler != null && autoCloseRunnable != null) {
+            handler.removeCallbacks(autoCloseRunnable);
+        }
+    }
+
+    public void onDestroy() {
+
+        if (handler != null && autoCloseRunnable != null) {
+            handler.removeCallbacks(autoCloseRunnable);
+        }
+
+        handler = null;
+        autoCloseRunnable = null;
     }
 }
